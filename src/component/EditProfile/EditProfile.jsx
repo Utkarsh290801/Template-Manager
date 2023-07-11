1314a7a877c203e9e1e5b6ca3e399e8dbda5f78e
Utkarsh290801 <utarora3@gmail.com>
2023-07-12 03:37:29 +0530

    ..

--- src/component/EditProfile/EditProfile.jsx
+++ src/component/EditProfile/EditProfile.jsx
@@ -1,8 +1,157 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Form, Col, Row, Toast } from "react-bootstrap";
 import "./EditProfile.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import { Formik } from "formik";
+import Swal from "sweetalert2";
+import * as Yup from "yup";
 const EditProfile = () => {
+  const navigate = useNavigate();
+  const { loggedIn, setloggedIn } = useContext(AppContext);
+  const url = "http://localhost:5000";
+  const [userArray, setUserArray] = useState([]);
+  const [updateForm, setUpdateForm] = useState({});
+  const [newPass, setNewPass] = useState("");
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user"))
+  );
+  // const [applyDetails, setApplyDetails] = useState({});
+  const [applyDetails, setApplyDetails] = useState(null);
+  useEffect(() => {
+    fetch(url + "/user/getbyid/" + currentUser._id)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUpdateForm(data);
+      });
+    console.log(currentUser);
+    fetch(url + "/apply/getbyuser/" + currentUser._id) // Fetch apply details
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setApplyDetails(data);
+      });
+  }, []);
+  const onFormSubmit = (value, { setSubmitting }) => {
+    fetch(url + "/user/update/" + currentUser._id, {
+      method: "PUT",
+      body: JSON.stringify(value),
+      // body : JSON.stringify({
+      //   password : newPass
+      // })
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then((data) => {
+          console.log(data);
+          setCurrentUser(data);
+          sessionStorage.setItem("user", JSON.stringify(data));
+        });
+      }
+      Swal.fire({
+        icon: "success",
+        title: "Welldone!",
+        text: "You have successfully Updated",
+      });
+    });
+  };
+  const onApplyFormSubmit = (value, { setSubmitting }) => {
+    fetch(url + "/apply/update/" + applyDetails._id, {
+      method: "PUT",
+      body: JSON.stringify(value),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error("Failed to update apply details");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setApplyDetails(data);
+        Swal.fire({
+          icon: "success",
+          title: "Well done!",
+          text: "You have successfully updated your apply details",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Failed to update apply details",
+        });
+      });
+  };
+  const passwordValidator = Yup.string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character"
+    );
+
+  const onChangePassword = () => {
+    if (!newPass) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Please enter a new password",
+      });
+      return;
+    }
+
+    if (!passwordValidator.isValidSync(newPass)) {
+      Swal.fire({
+        icon: "error",
+        title: "Please check the requirements.",
+        text: "Must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character",
+      });
+      return;
+    }
+    fetch(url + "/user/update/" + currentUser._id, {
+      method: "PUT",
+      body: JSON.stringify({
+        password: newPass,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error("Failed to change password");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setCurrentUser(data);
+        sessionStorage.setItem("user", JSON.stringify(data));
+        Swal.fire({
+          icon: "success",
+          title: "Well done!",
+          text: "Password changed successfully",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Failed to change password",
+        });
+      });
+  };
+
   return (
     <div>
       <div className="bookForm ">
@@ -31,60 +180,207 @@ const EditProfile = () => {
             <div className="priceInput">99</div>
           </Col>
         </Row> */}
-
-        <Form>
-          <Row>
-            <Col md={6} xs={12}>
-              <Form.Group>
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Full Name
-                </Form.Label>
-                <Form.Control type="text" placeholder="Your Name" />
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label style={{ fontWeight: "bold" }}>Email</Form.Label>
-                <Form.Control type="text" placeholder="Email Address" />
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label style={{ fontWeight: "bold" }}>Address</Form.Label>
-                <Form.Control type="text" placeholder="Address" />
-              </Form.Group>
-            </Col>
-            <Col md={6} xs={12}>
-              <div>
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Card Number
-                </Form.Label>
+        <Formik
+          enableReinitialize={true}
+          initialValues={currentUser}
+          onSubmit={onFormSubmit}
+        >
+          {({ values, handleChange, handleSubmit, isSubmitting, errors }) => (
+            <Form onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6} xs={12}>
+                  <Form.Group className="mt-3">
+                    <Form.Label style={{ fontWeight: "bold" }}>
+                      Full Name
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Your Name"
+                      name="username"
+                      onChange={handleChange}
+                      value={values.username}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group className="mt-3">
+                    <Form.Label style={{ fontWeight: "bold" }}>
+                      Email
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Email Address"
+                      onChange={handleChange}
+                      value={values.email}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <div className="text-center">
+                <button className="mainBtn mt-4" type="submit">
+                  Update Profile
+                </button>
               </div>
-              <div className="mt-3">
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Expiration Date
-                </Form.Label>
-              </div>
-              <div className="mt-3">
-                <Form.Label style={{ fontWeight: "bold" }}>CVC</Form.Label>
-              </div>
-            </Col>
-          </Row>
+            </Form>
+          )}
+        </Formik>
+        {applyDetails && (
+          <Formik
+            enableReinitialize={true}
+            initialValues={applyDetails}
+            onSubmit={onApplyFormSubmit}
+          >
+            {({ values, handleChange, handleSubmit, isSubmitting }) => (
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <Col md={6} xs={12}>
+                    <Form.Group className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Mobile Number
+                      </Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Enter Number.."
+                        onChange={handleChange}
+                        name="number"
+                        value={values.number}
+                      />
+                    </Form.Group>
+
+                    <Form.Group>
+                      <div className="mt-3">
+                        <Form.Label style={{ fontWeight: "bold" }}>
+                          Gender
+                        </Form.Label>
+                        <div className="d-flex align-items-center justify-content-around">
+                          {" "}
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="Male"
+                            checked={values.gender === "Male"}
+                            onChange={handleChange}
+                          />{" "}
+                          Male
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="Female"
+                            checked={values.gender === "Female"}
+                            onChange={handleChange}
+                          />{" "}
+                          Female
+                        </div>
+                      </div>
+                    </Form.Group>
+                    <div className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        College
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="College"
+                        onChange={handleChange}
+                        value={values.college}
+                        name="college"
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Course
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Course"
+                        name="course"
+                        onChange={handleChange}
+                        value={values.course}
+                      />
+                    </div>
+                  </Col>
+                  <Col md={6} xs={12}>
+                    <div className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Stream
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Stream"
+                        name="stream"
+                        onChange={handleChange}
+                        value={values.stream}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Passing Year
+                      </Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Year"
+                        name="year"
+                        onChange={handleChange}
+                        value={values.year}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Github Profile Link
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Github Link.."
+                        name="github"
+                        onChange={handleChange}
+                        value={values.github}
+                      />
+                    </div>
+                    <div className="mt-2">
+                      <Form.Label style={{ fontWeight: "bold" }}>
+                        Linkedin Profile Link
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Linkedin Link.."
+                        name="linkedin"
+                        onChange={handleChange}
+                        value={values.linkedin}
+                      />
+                    </div>
+                  </Col>
+                </Row>
+                <div className="text-center">
+                  <button className="mainBtn mt-4" type="submit">
+                    Update Details
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        )}
+        <div className="mt-3">
+          <Form.Group>
+            <Form.Label style={{ fontWeight: "bold" }}>
+              Change Password
+            </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Change Password"
+              name="password"
+              onChange={(e) => setNewPass(e.target.value)}
+              value={newPass}
+            />
+          </Form.Group>
           <div className="text-center">
-            <button className="mainBtn mt-4" type="submit">
-              Update Profile
-            </button>
-          </div>
-          <div className="mt-3">
-            <Form.Group>
-              <Form.Label style={{ fontWeight: "bold" }}>Change Password</Form.Label>
-              <Form.Control type="text" placeholder="Change Password" />
-            </Form.Group>
-            <div className="text-center">
-            <button className="mainBtn mt-4" type="submit">
+            <button
+              className="mainBtn mt-4"
+              type="button"
+              onClick={onChangePassword}
+            >
               Update Password
             </button>
           </div>
-          </div>
-        </Form>
+        </div>
       </div>
     </div>
   );
